1186e92254f717fe38ed9a8cf2e0053504d8e4dc
gmmteams <69051835+gmmteams@users.noreply.github.com>
2020-10-21 15:02:07 +0530

    Update GMMController.java

--- src/main/java/com/gmm/gmmapp/resources/GMMController.java
+++ src/main/java/com/gmm/gmmapp/resources/GMMController.java
@@ -11,6 +11,6 @@ public class GMMController {
 
 	@GetMapping
     public String sayHello() {
-        return "Hello and Welcome to the GMM application4";
+        return "Hello and Welcome to the GMM application5";
     }
 }
